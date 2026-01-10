2b92f7dec5d3babd19cb80518bee2fc483b22f37
Kumar-Munna-dev <fullstacks.dev@gmail.com>
2026-01-10 11:32:36 +0530

    fixed robots.txt file

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,12 @@
-export default function robots() {
-  return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
-    sitemap: 'https://acme.com/sitemap.xml',
-  }
+export default function robots() { return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
+    sitemap: 'https://a2tool.com/sitemap.xml',
+    host: 'https://a2tool.com',
+  };
 }
